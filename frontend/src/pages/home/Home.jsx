a4676a63871fa17edc2736a78c3e043a16843e49
masumparvez56 <parvezmasum581@gmail.com>
2025-10-07 12:08:29 +0600

    added NewsLetter Section

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../../components/Banner';
 import BlogList from '../blogs/BlogList';
+import NewsLetter from './NewsLetter';
 
 const Home = () => {
     return (
         <section className='section-container'>
            <Banner/>
            <BlogList/>
+           <NewsLetter/>
         </section>
     );
 };
---
+++ frontend/src/pages/home/NewsLetter.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import newsLetterImg from '../../assets/news-letter.png'
+
+const NewsLetter = () => {
+    return (
+        <div className='my-16 bg-no-repeat bg-cover bg-center rounded-md' style={{ backgroundImage: `url(${newsLetterImg})` }}>
+            <div className='py-12 bg-black/40 rounded-md h-full flex items-center justify-center text-center'>
+                <div className='space-y-3 p-4'>
+                    <h2 className='text-2xl font-bold text-white'>Sign up for our newsletter</h2>
+                    <p className='text-lg text-white'> Get the latest news, updates, and offers straigth to your inbox</p>
+                    <button className='bg-white hover:bg-secondary hover:text-white text-primary font-bold py-2 px-8 rounded-full'>Sign up</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NewsLetter;
